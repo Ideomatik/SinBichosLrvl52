527aac0170ed9c214a5f90d0a35ab32c311f3797
Ideomatik <ideomatik@gmail.com>
2016-06-07 20:53:38 -0300

    Actualizacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 55</div>
+                <div class="title">Laravel 515</div>
             </div>
         </div>
     </body>
